dccd1887daea5fe43c37371a3a580cab89bf79ef
星空糖 <haotian0425@foxmail.com>
2019-10-23 16:59:26 +0800

    修改了一些问题，加入了一个新问题的思考

--- ch04/Localize/TestLocation.java
+++ ch04/Localize/TestLocation.java
@@ -12,15 +12,12 @@ public class TestLocation {
         Scanner input = new Scanner(System.in);
         int row = input.nextInt();
         int col = input.nextInt();
-        if(input.hasNext())
-            input.next();   //清空后续的内容
+        //如何清空后来的不需要的内容呢？
         double[][] a = new double[row][col];
         for(int i = 0;i < row; i++) {
             System.out.printf("请输入第%d行的数据（以前%d个为准，多输入无效）：", i + 1, col);
             for(int j = 0; j < col; j++)
                 a[i][j] = input.nextDouble();
-            if(input.hasNext())
-                input.next();   //清空后续的内容
         }
         Location position = Location.locationLargest(a);
         System.out.print(position.toSring());
